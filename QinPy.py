dfa07d202bdd46657a18722b44df76b85b729d81
Angyalka Valcsics <36708283+angyalkavalcsics@users.noreply.github.com>
2022-08-29 18:57:32 -0400

    Init Markovian model mm1

--- QinPy.py
+++ QinPy.py
@@ -1,4 +1,7 @@
 import numpy as np
+import sys
+from tabulate import tabulate
+
 def solverouting(R):
     n = R.shape[1]
     I = np.identity(n)
@@ -8,73 +11,39 @@ def solverouting(R):
     v = np.matmul(np.matmul(b, Q_t), np.linalg.inv(np.matmul(Q, Q_t)))
     return(v)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def mmc(lam, mu, c, tol):
+    # Single Server Markovian Model
+    if(c == 1): 
+        # Traffic intensity
+        rho = lam/mu
+        if(rho < 1): print("System is stable.")
+        else:
+            sys.exit("System is unstable.")
+        # pn := steady state probability of n in the system.
+        # That is, p0 is the probability of no entities in system,
+        # p1 is the probability of one entity in the system, and so on.
+        pn = []
+        n = 0
+        while(np.sum(pn) < (1-tol)):
+            # Uses the geometric probability function
+            pn.append((rho**n)*(1-rho))
+            n += 1
+        # Mean number in system
+        L = rho/(1-rho)
+        # Mean number in queue
+        Lq = (rho**2)/(1-rho)
+        # Mean time in system
+        W = 1/(mu-lam)
+        # Mean time in queue
+        Wq = rho/(mu - lam)
+        # Idle time
+        I = 1-rho
+        res = [["Traffic intensity", rho],
+              ["Idle time", I],
+              ["Mean time in system", W],
+              ["Mean time in queue", Wq],
+              ["Mean number in system", L],
+              ["Mean number in queue", Lq]]
+        print(tabulate(res, headers=["Definition", "Result"]))
+        
+        return(pn, rho, L, Lq, W, Wq, I)
